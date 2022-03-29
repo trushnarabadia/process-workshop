d23f0c224fcba187c009141850750b20c77d7dad
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-29 12:18:18 +0100

    leapyear class created

---
+++ lib/leap_year.rb
@@ -0,0 +1,3 @@
+class LeapYear
+
+end
---
+++ spec/leap_year_spec.rb
@@ -0,0 +1,19 @@
+# A simple tool that will tell you whether a year is a leap year, according to 
+# the following rules:
+
+# All years divisible by 400 ARE leap years (e.g. 2000 was a leap year)
+
+# All years divisible by 100 but not by 400 are NOT leap years (e.g. 1700, 1800 
+# and 1900 were not leap years)
+
+# All years divisible by 4 and not by 100 ARE leap years (e.g. 2004, 2008 and 
+# 2012 were leap years)
+
+# All years not divisible by 4 are NOT leap years (e.g. 2009, 2010 and 2011 were 
+# not leap years)
+
+require_relative "../lib/leap_year"
+
+describe LeapYear do
+
+end
